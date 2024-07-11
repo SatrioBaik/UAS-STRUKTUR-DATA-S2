77e181d9681d886807b5987a97549baa123e3791
budicuy <personal.budicuy@gmail.com>
2024-07-11 16:25:18 +0800

    Membuat project aplikasi perpustakaan digital

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void menu();
+
+int main()
+{
+    menu();
+    return 0;
+}
+
+void menu()
+{
+    cout << "~~~~~~~~~~~~~ MENU ~~~~~~~~~~~~~" << endl;
+    cout << "1. Menu BUKU" << endl;
+    cout << "2. Menu BUKU" << endl;
+    cout << "3. Menu BUKU" << endl;
+    cout << "4. Menu BUKU" << endl;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+
+    cout << "Pilih Menu : ";
+    int pilihan;
+    cin >> pilihan;
+}
